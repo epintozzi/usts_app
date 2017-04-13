76cce6ccfb77121955673dda819f03f4ef04cea8
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-04-13 15:32:51 -0600

    Add flash messages to application.html Update user registration test to match devise provided routes Generate devise views

--- spec/features/users/user_can_create_account_spec.rb
+++ spec/features/users/user_can_create_account_spec.rb
@@ -5,18 +5,16 @@ describe "user sign up" do
     visit root_path
     click_on "Sign Up"
 
-    expect(current_path).to eq(new_user_path)
+    expect(current_path).to eq(new_user_registration_path)
     fill_in "user[email]", with: "erin@email.com"
     fill_in "user[first_name]", with: "Erin"
     fill_in "user[last_name]", with: "Pintozzi"
     fill_in "user[password]", with: "password1"
     fill_in "user[password_confirmation]", with: "password1"
     # fill_in "user[driver?]", with: "yes" Change to a select yes/no for driver
-    click_on "Create User"
+    click_on "Sign up"
 
-    user = User.last
-
-    expect(page).to have_content("You have successfully created an account")
-    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(current_path).to eq(root_path)
   end
 end
