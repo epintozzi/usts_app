2e3b9b9b4dfbd58efcadf94df3465a012e45c525
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-04-17 12:27:24 -0600

    add News table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417172438) do
+ActiveRecord::Schema.define(version: 20170417181721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "news", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "image"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_news_on_author_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "first_name"
@@ -37,4 +47,5 @@ ActiveRecord::Schema.define(version: 20170417172438) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "news", "users", column: "author_id"
 end
---
+++ spec/factories/news.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :news do
+    title "MyString"
+    content "MyText"
+    image "MyString"
+    user nil
+  end
+end
