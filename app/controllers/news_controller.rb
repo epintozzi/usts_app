1cd922dd0d27f72ac27d46c1d982e82135447b1c
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-04-17 13:32:51 -0600

    Add "create news" form Add News show view Add routes for news index, show, new, create Test creation of news article

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,32 @@
+class NewsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @news = News.find(params[:id])
+  end
+
+  def new
+    @news = News.new()
+  end
+
+  def create
+    @news = News.new(news_params)
+    @news.author = current_user
+    if @news.save
+      flash[:success] = "Your news post has been successfully created."
+      redirect_to news_path(@news)
+    else
+      flash[:alert] = "Something went wrong. Please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:id, :title, :content, :author_id)
+  end
+
+end
