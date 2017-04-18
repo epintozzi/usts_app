30b3cbe850f5af6d53e958dab0a2b1c01235426c
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-04-18 12:11:12 -0600

    Add hotel info to races

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417223825) do
+ActiveRecord::Schema.define(version: 20170417230206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +41,11 @@ ActiveRecord::Schema.define(version: 20170417223825) do
     t.float    "latitude"
     t.datetime "start_date"
     t.datetime "end_date"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.text     "sanction"
     t.text     "details"
+    t.text     "hotel_information"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/models/race_spec.rb
+++ spec/models/race_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe Race, type: :model do
     end
     context "valid attributes" do
       it "is valid with all attributes" do
-        race = Race.new(street_address: "123 Lake St", city: "DePue", state: "IL", start_date: "2017-07-26 16:33:57", end_date: "2017-07-30 16:33:57", sanction: "sanction.pdf", details: "details go here", fee_override: 50, latitude: 89.1234, longitude: 40.1234)
+        race = Race.new(street_address: "123 Lake St", city: "DePue", state: "IL", start_date: "2017-07-26 16:33:57", end_date: "2017-07-30 16:33:57", sanction: "sanction.pdf", details: "details go here", fee_override: 50, latitude: 89.1234, longitude: 40.1234, hotel_information: "hotel info here")
 
         expect(race).to be_valid
       end
