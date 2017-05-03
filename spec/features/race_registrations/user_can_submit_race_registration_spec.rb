c5cf070297abc500295f087e4c17c04cfa0ff1af
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-05-02 18:08:10 -0600

    Add custom error message for when user tries to register for same race and boat class more than once

--- spec/features/race_registrations/user_can_submit_race_registration_spec.rb
+++ spec/features/race_registrations/user_can_submit_race_registration_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 describe "race_registrations/new" do
   before :each do
-    create(:boat_class, class_name: "125cc Runabout")
-    create(:race, title: "Race for the Kids", city: "Lake Alfred")
-    create(:usts_registration, first_name: "Erin")
+    @boat_class = create(:boat_class, class_name: "125cc Runabout")
+    @race = create(:race, title: "Race for the Kids", city: "Lake Alfred")
+    @usts_reg = create(:usts_registration, first_name: "Erin")
     user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -93,5 +93,24 @@ describe "race_registrations/new" do
     click_on "Submit Race Registration"
 
     expect(page).to have_content("Something went wrong. Boat number can't be blank. Please try your registration again.")
+  end
+
+  scenario "user sees an error if they try to register for same race location and class" do
+
+    create(:race_registration, usts_registration: @usts_reg, boat_class: @boat_class, race: @race)
+
+    select "Erin Pintozzi"
+    select "Lake Alfred"
+    select "125cc Runabout"
+    fill_in "race_registration[boat_number]", with: "V26"
+    fill_in "race_registration[boat_name]", with: "Ronda"
+    fill_in "race_registration[boat_owner]", with: "Bill Kurps"
+    fill_in "race_registration[boat_owner_zip]", with: "60402"
+    fill_in "race_registration[emergency_contact]", with: "Brad Barth"
+    fill_in "race_registration[emergency_phone]", with: "9876544321"
+    check "race_registration[emergency_contact_at_race]"
+    click_on "Submit Race Registration"
+
+    expect(page).to have_content("Something went wrong. Boat class has already been registered for this driver and race location. Please try your registration again.")
   end
 end
