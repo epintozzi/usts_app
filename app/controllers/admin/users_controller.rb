ce2de9743aec9e899941e89d6803670ba12b05c7
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-05-15 14:06:53 -0600

    Create feature for admin to edit a user account

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,23 @@ class Admin::UsersController < Admin::BaseController
     @users = User.all
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "This user has successfully been updated."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "Something went wrong. #{@user.errors.full_messages.join(' ')}. Please try again."
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:id, :first_name, :last_name, :email, :role)
+  end
+
 end
