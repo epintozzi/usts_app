a1c957e983267bcf22281bf9454e9a60fb2eb144
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-05-18 14:11:59 -0600

    Change news image from string to paperclip attachment attribute for future use

--- app/models/news.rb
+++ app/models/news.rb
@@ -6,4 +6,7 @@ class News < ApplicationRecord
   validates :published_on, presence: true
   validates :author, presence: true
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518194458) do
+ActiveRecord::Schema.define(version: 20170518200848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,14 @@ ActiveRecord::Schema.define(version: 20170518194458) do
   create_table "news", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.string   "image"
     t.integer  "author_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.datetime "published_on"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["author_id"], name: "index_news_on_author_id", using: :btree
   end
 
