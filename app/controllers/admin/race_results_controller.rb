60c88fc806a9648132561f38d848df0218affa16
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-06-01 18:03:31 -0600

    Update display of race results for admin

--- app/controllers/admin/race_results_controller.rb
+++ app/controllers/admin/race_results_controller.rb
@@ -1,7 +1,9 @@
 class Admin::RaceResultsController < Admin::BaseController
 
   def index
-    @results = RaceResult.all
+    @boat_classes = BoatClass.all
+    @races = Race.all.order(start_date: :asc)
+    # @results = RaceResult.all
     # @results = RaceResult.group(:boat_class).count
   end
 
