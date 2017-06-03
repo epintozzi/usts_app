b8146526dd78bdb79ad0350c86d9275eb73d47a4
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-06-02 19:08:08 -0600

    Display race and usts registrations on shopping cart Do light styling of shopping cart table

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -2,6 +2,7 @@ class ShoppingCartController < ApplicationController
 
   def show
     @unpaid_race_registrations = RaceRegistration.unpaid_race_reg(current_user)
+    @unpaid_usts_registrations = UstsRegistration.unpaid_usts_reg(current_user)
   end
 
 
