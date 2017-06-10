940db6208dcd702865c8fcff9aee03880a4a273b
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-06-09 18:01:52 -0600

    Add previous and next links for photos as temp workaround

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,4 +6,12 @@ class Photo < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
   validates_attachment_presence :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def next
+    Photo.where("id > ?", id).limit(1).first
+  end
+
+  def previous
+    Photo.where("id < ?", id).limit(1).first
+  end
 end
