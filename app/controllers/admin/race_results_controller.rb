beda3e1e3f34acd795e744d79acd6642ec5c1626
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-08-07 17:18:07 -0700

    add edit ability on individual race_results for admin

--- app/controllers/admin/race_results_controller.rb
+++ app/controllers/admin/race_results_controller.rb
@@ -5,9 +5,34 @@ class Admin::RaceResultsController < Admin::BaseController
     @races = Race.all.order(start_date: :asc)
   end
 
+  def edit
+    # binding.pry
+    @race_result = RaceResult.find(params[:id])
+  end
+
+  def update
+    @race_result = RaceResult.find(params[:id])
+    if @race_result.update(race_result_params)
+      flash[:success] = "You have successfully updated this race result"
+      redirect_to admin_race_results_path
+    else
+      flash[:danger] = "Something went wrong. #{@race_result.errors.full_messages.join(' ')}. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
   def import
     RaceResult.import(params[:file])
     redirect_to admin_race_results_path, notice: "Results imported"
+  end
+
+  private
+
+  def race_result_params
+    params.require(:race_result).permit(:id, :race_id, :boat_class_id, :driver_name, :points)
   end
 
 end
---
+++ spec/features/admin/race_results/admin_can_edit_race_result_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "/admin/race_results" do
+  scenario "admin can edit individual race result" do
+    admin = create(:user, role: 2)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    race_1 = create(:race, city: "Denver")
+    race_2 = create(:race, city: "Atlanta")
+    boat_class_1, boat_class_2 = create_list(:boat_class, 2)
+    race_result = create(:race_result, driver_name: "Erin", race_id: race_1.id, boat_class_id: boat_class_1.id)
+
+    visit admin_race_results_path
+    click_on "Edit"
+
+    expect(current_path).to eq(edit_admin_race_result_path(race_result))
+
+    fill_in "race_result[driver_name]", with: "Ryan"
+    select boat_class_2.class_name
+    select race_2.city
+    fill_in "race_result[points]", with: 10
+    click_on "Update Race result"
+
+    expect(current_path).to eq(admin_race_results_path)
+    expect(page).to have_content("You have successfully updated this race result")
+    expect(page).to have_content("Ryan")
+    expect(page).to_not have_content("Erin")
+  end
+end
